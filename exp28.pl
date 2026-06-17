249d386efbee5783d93a264ee4edd9d19d4ef845
hamuedp-cloud <hamuedp@gmail.com>
2026-06-17 09:56:29 +0530

    Add files via upload

---
+++ exp28.pl
@@ -0,0 +1,26 @@
+% Symptoms
+has_symptom(fever).
+has_symptom(cough).
+has_symptom(rash).
+has_symptom(conjunctivitis).
+has_symptom(runny_nose).
+
+% Disease rules
+hypothesis(measles) :-
+    has_symptom(fever),
+    has_symptom(cough),
+    has_symptom(conjunctivitis),
+    has_symptom(runny_nose),
+    has_symptom(rash).
+
+hypothesis(flu) :-
+    has_symptom(fever),
+    has_symptom(cough),
+    has_symptom(runny_nose).
+
+hypothesis(common_cold) :-
+    has_symptom(runny_nose),
+    has_symptom(cough).
+
+diagnose(Disease) :-
+    hypothesis(Disease).
